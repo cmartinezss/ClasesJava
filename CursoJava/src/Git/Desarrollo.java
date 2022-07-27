3333e84cc4c89990d883ab6260bb08d74f524da2
cmartinezs <cmartinezs@11LAPBS9RPN3.indra.es>
2022-07-27 10:41:54 +0200

    Primera subida, con comentario del ejercicio

--- CursoJava/src/Git/Desarrollo.java
+++ CursoJava/src/Git/Desarrollo.java
@@ -1,4 +1,23 @@
+/*
+ * Acceder mediante el cmd (o su equivalente) del sistema al repositorio local de su proyecto y generar una nueva rama que se llame 
+"DESARROLLO"
+Luego, generar un programa sobre dicha rama desde el IDE y actualizarlo en el repositorio remoto (Github). 
+El programa debe hacer lo siguiente:
+Desarrollar un bot que te pregunte como te sentis el día de hoy, y en base a las siguientes posibilidades haga una accion
+1. Triste -> Cuenta un chiste
+2. Alegre -> "Enhorabuena"
+3. Nervioso -> Mensaje de calma
+TIP: A los chistes los pueden almacenar en un array y usar el metodo RANDOM para contar un chiste aleatorio. 
+El programa tiene que tener un mensaje de bienvenida, donde te pregunte tu nombre previamente y te informe la fecha actual.
+ENTREGABLES: 
+Link del repositorio con el codigo almacenado. 
+Captura del CMD mostrando la generacion de la rama. 
+(!) EL CODIGO DEBE ESTAR ALMACENADO EN LA RAMA "DESARRROLLO (!)
+ */
+
 package Git;
+
+
 
 public class Desarrollo {
 	public static void main (String[] args) {
